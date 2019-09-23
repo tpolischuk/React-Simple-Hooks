86e7369c1ca5424a98fe25dd59cfbe8ab8012a29
Trevor Polischuk <trevor.polischuk@gmail.com>
2019-09-23 16:37:21 -0700

    Refactoring class to function and hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-class App extends React.Component {
+const App = () => {
 
-    state = { resource: 'posts' };
+    const [resource, setResource] = useState('posts');
 
-    render() {
-        return (
+    return (
+        <div>
             <div>
-                <div>
-                    <button onClick={() => this.setState({ resource: 'posts' })}>Posts</button>
-                    <button onClick={() => this.setState({ resource: 'todos' })}>Todos</button>
-                </div>
-                {this.state.resource}
+                <button onClick={() => setResource('posts')}>Posts</button>
+                <button onClick={() => setResource('todos')}>Todos</button>
             </div>
-        );
-    }
+            {resource}
+        </div>
+    );
+
 }
 
 export default App
