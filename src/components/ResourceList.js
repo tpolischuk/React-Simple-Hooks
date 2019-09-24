5c8a2514c18a22fdf9e4f829599a1a81888ca35d
Trevor Polischuk <trevor.polischuk@gmail.com>
2019-09-24 16:47:07 -0700

    Rendering the list

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -5,19 +5,20 @@ const ResourceList = ({ resource }) => {
 
     const [resources, setResources] = useState([]);
 
-    const fetchResource = async (resource) => {
-        const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`)
-
-        setResources(response.data);
-    }
 
 
     // The value passed to the array is used to compare the data, only runs if its different
     useEffect(() => {
-        fetchResource(resource);
+        (async (resource) => {
+            const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`)
+
+            setResources(response.data);
+        })(resource)
     }, [resource]);
 
-    return <div>{resources.length}</div>;
+    return (
+        <ul>{resources.map(record => <li key={record.id}>{record.title}</li>)}</ul>
+    );
 }
 
 export default ResourceList;
